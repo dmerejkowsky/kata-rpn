ea98ba8ce622cc6080c36eab2678a2b89bd309ab
Your Name <fcharvet0901@gmail.com>
2023-11-08 10:59:45 +0100

    basic test

--- src/Main/Answer.java
+++ src/Main/Answer.java
@@ -1,17 +1,40 @@
 package src.Main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Answer {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Write down a new expression:");
-        String expression = sc.nextLine();
+        ArrayList<String> operands = getOperation();
 
-        String[] operands = expression.split(" ");
+        if(operands.isEmpty()) {
+            System.out.println("Expression vide.");
+            return;
+        }
+
+        String operator = operands.get(operands.size() - 1);
+        operands.remove(operands.size() - 1);
 
         for (String operand : operands) System.out.println(operand);
     }
 
+    public static ArrayList<String> getOperation() {
+        System.out.println("Write down a new expression:");
+        String expression = sc.nextLine();
+
+        return new ArrayList<>(Arrays.asList(expression.split(" ")));
+    }
+
+    public double calc(ArrayList<String> operands, String operator) {
+        int result = 0;
+        if(operator.equals("+")) {
+            for (String operand : operands) result += Double.parseDouble(operand);
+            return result;
+        }
+
+        throw new ArithmeticException();
+    }
 }
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,6 +1,22 @@
 package src.Test;
 
-public class Test {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import src.Main.Answer;
 
+import java.util.ArrayList;
 
+class AnswerTest {
+
+    @Test
+    void calcTest() {
+        Answer answerTest = new Answer();
+
+        ArrayList<String> expression = new ArrayList<>();
+        expression.add("1");
+        expression.add("8");
+
+        double value = answerTest.calc(expression, "+");
+        Assertions.assertEquals(9., value);
+    }
 }
