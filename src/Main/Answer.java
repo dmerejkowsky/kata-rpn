8559b8a73361775b59758d9521d5a011185975bf
Your Name <fcharvet0901@gmail.com>
2023-11-08 10:13:23 +0100

    Splitting expression in operands

--- src/Main/Answer.java
+++ src/Main/Answer.java
@@ -1,8 +1,17 @@
 package src.Main;
 
+import java.util.Scanner;
+
 public class Answer {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Write down a new expression:");
+        String expression = sc.nextLine();
+
+        String[] operands = expression.split(" ");
+
+        for (String operand : operands) System.out.println(operand);
     }
 
 }
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,4 +2,5 @@ package src.Test;
 
 public class Test {
 
+
 }
